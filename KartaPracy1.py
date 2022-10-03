d166aef6dbc34240a68dd3d498eb327f71ed1028
McCherryy <114579421+McCherryy@users.noreply.github.com>
2022-10-03 11:21:04 +0000

    zrobiono 1-4 KP1

---
+++ KartaPracy1.py
@@ -0,0 +1,22 @@
+#zad1
+
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+#zad2
+
+# a = int(input())
+# b = int(input())
+# print((a + b) **2)
+
+#zad3
+
+# a = int(input())
+# b = int(input())
+# print((a - b) **3)
+
+#zad4
+
+# a, b, c = map(int,input().split())
+# print(a *b *c)
